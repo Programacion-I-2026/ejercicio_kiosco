8e34ce69757b910306470f27c288d52fd34f8684
nvasone <nicolasvasone10@gmail.com>
2026-04-08 11:29:03 -0300

    cambio del papuh

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ total = precio + cantidad
 impuesto = total * 10 
 total_final = total + impuesto
 print(total_final)
+#el error de esto es que suma el precio y la cantidad en vez de multiplicarla, tambien multiplica por 10 cuando debe dividir
 
