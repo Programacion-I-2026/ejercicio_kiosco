a779b5cc82e9ed4a293e1a9ccfd82522cccfe4f4
danilo <danilojosegutierrez020@gmail.comgit config --global user.name>
2026-04-13 09:55:06 -0300

    gutierrez

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 precio = 50
 cantidad = 4
-total = precio + cantidad 
-impuesto = total * 10 
+total = precio * cantidad #cambiamos la suma por una miltiplicacion
+impuesto = total * 0.1 #sirve para que se pueda sacar el 10% que nos pide          
 total_final = total + impuesto
 print(total_final)
