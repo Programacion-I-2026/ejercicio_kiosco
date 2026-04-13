d607a192b93cf71bf24f5db4d8d32cf272118425
juan <juancinta2@gmail.com>
2026-04-13 08:45:38 -0300

    cinta

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 precio = 50
-cantidad = 4
-total = precio + cantidad 
-impuesto = total * 10 
+cantidad = 4 
+total = precio * cantidad #cambiar el (+) por (*) 
+impuesto = total * 1.10 #1.10 #cambiar el (10) por (1.10)
 total_final = total + impuesto
 print(total_final)
