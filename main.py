a290af0c1fecb42dfb9048823ac5543a68d917d9
Giselle Iriarte <giseiriarte21@gmail.com>
2026-04-13 11:42:33 -0300

    Fix total calculation and update impuesto logic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 precio = 50
 cantidad = 4
-total = precio + cantidad 
-impuesto = total * 10
+total = precio *cantidad 
+impuesto = total * 0.10 
 total_final = total + impuesto
-print(total_final)
+print("el cliente pago:", total_final) 
