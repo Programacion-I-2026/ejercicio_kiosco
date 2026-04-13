8e3c37c8af2acd547763aa2291015a9c74225f03
unknown <rocofoco07@gmail.com>
2026-04-13 09:27:41 -0300

    ejercicio corregido

---
+++ ejercicioCorregido (1).py
@@ -0,0 +1,9 @@
+precio = 50
+cantidad = 4
+total = precio * cantidad
+#corregi el mas por una multiplicacion entre precio y cantidad
+impuesto = total / 10
+#corregi la multiplicacion por una division entre el total y el impuesto
+total_final = total + impuesto
+print(total_final)
+
