1db31598bdf1361812980e52e8dc924f3582b052
Teromax34 <teromax34@gmail.com>
2026-04-13 08:32:03 -0300

    agregado correción

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+
 precio = 50
 cantidad = 4
+#deberia ser precio * cantidad
+#total = precio * cantidad
 total = precio + cantidad 
+#en lugar de total * 10 deberia ser total * 0.1 que deberia dar el 10% del total
+#impuesto = total * 0.1
 impuesto = total * 10 
 total_final = total + impuesto
 print(total_final)
+
