7972a45b390afcb8c58699513276945bdcd16f9a
ramiro <ramichavez667@gmail.com>
2026-04-13 09:33:33 -0300

    Chavez

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 precio = 50
 cantidad = 4
-total = precio + cantidad 
-impuesto = total * 10 
+total = precio * cantidad       #Aqui hay que multiplicar la cantidad con el precio de esas 4 NO sumar.
+impuesto = total * 0.10    #Aca pide el 10% no tiene sentido la multiplicacion
 total_final = total + impuesto
 print(total_final)
