c447ebd1d145b9e25ba3b5e822e8a466cef7f0a6
Zak289 <joaquinismaelaguirre0@gmail.com>
2026-04-13 10:02:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 precio = 50
 cantidad = 4
-total = precio + cantidad 
-impuesto = total * 10
+total = precio * cantidad 
+impuesto = total * 10/100
 total_final = total + impuesto
 print(total_final)
