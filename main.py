daa365177c4a7f0562c5183869a6d949b7e2ef03
Renata <renataserafini.123@gmail.com>
2026-04-13 08:52:05 -0300

    Resolucion Ejercicio Kiosco

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 precio = 50
 cantidad = 4
 total = precio + cantidad 
+#El primer error es que el total debe calcularse multiplicando el precio por la cantidad, quedando de esta manera:
+#total = precio * cantidad
 impuesto = total * 10 
+#El segundo error es que el impuesto debe calcularse sacando el 10% sobre el total.
+#impuesto = total * 0.10
 total_final = total + impuesto
 print(total_final)
