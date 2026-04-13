7310e94bd2e5a142dcad209614e2a432e67252d5
Franco Eroles <francoeroles7@gmail.com>
2026-04-13 09:55:45 -0300

    corregido calculo total e impuesto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 precio = 50
 cantidad = 4
-total = precio + cantidad 
-impuesto = total * 10 
+total = precio * cantidad 
+impuesto = total * 0.10 
 total_final = total + impuesto
-print(total_final)
+print("el precio final es",total_final)
+
+
