36b6150f2d7478c6c97b0ce54bb9dae3c1e78223
nvasone <nicolasvasone10@gmail.com>
2026-04-13 09:23:54 -0300

    modifique el archivo

--- main modificado.py
+++ main modificado.py
@@ -1,8 +1,9 @@
 precio = 50
 cantidad = 4
-total = precio + cantidad 
-impuesto = total * 10 
+total = precio * cantidad 
+impuesto = total * 1.10 
 total_final = total + impuesto
 print(total_final)
 #el error de esto es que suma el precio y la cantidad en vez de multiplicarla, tambien multiplica por 10 cuando debe dividir
-
+# cambiamos la suma por la multipliacion 
+#para sacar el 10%
