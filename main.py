f277324db1e2be864c80c093dedf26c848c067b6
Delfina <delfiinegro@gmail.com>
2026-04-14 17:29:31 -0300

    Corrección ejercicio kiosko

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 precio = 50
 cantidad = 4
-total = precio + cantidad 
-impuesto = total * 10
+
+#El total no es suma, es multiplicación (precio * cantidad).
+total = precio * cantidad 
+
+#El impuesto no se multiplica por 10, para sacar el 10% hay que usar (total * 10) / 100
+impuesto = (total * 10) / 100
+
 total_final = total + impuesto
 print(total_final)
