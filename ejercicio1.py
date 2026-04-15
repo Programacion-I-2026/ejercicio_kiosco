7f11b63ce1cedb9f74fd232672f0ca4804d7f4e4
juan <juancinta2@gmail.com>
2026-04-15 09:16:47 -0300

    problema 1, con try-except

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,6 +4,12 @@ bebida_ca = int(input("Ingrese el total de la bebida con alcohol:"))
 invitados = int(input("Ingrese la cantidad de invitados: "))
 invitados_ca = int(input("Ingrese la cantidad de invitados con alcohol: "))
 importe = comida/invitados+bebida_sa/invitados
-extra = bebida_ca/invitados_ca
+try : 
+    extra = bebida_ca/invitados_ca
+except:
+    extra = 0
+    print("No hay personas que consuman alcohol")
+else:
+    print("El importo total de las personas que toman alcohol: ", extra)
+
 print("El importe que debe abonar es de: ",importe)
-print("El importe que debe abonar las personas que van a tomar con alcohol es de : ",extra)
