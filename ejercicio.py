c3a0c017781f41c4e755654980b0c027c9a99a09
Delfina <delfiinegro@gmail.com>
2026-04-15 09:47:12 -0300

    ejercicio bebidas (3)

--- ejercicio.py
+++ ejercicio.py
@@ -1,16 +1,21 @@
-comida = int(input("Ingrese el total de la comida: "))
-bebida_sa = int(input("Ingrese el total de la bebida sin alcohol: "))
-bebida_ca = int(input("Ingrese el total de la bebida con alcohol: "))
-invitados = int(input("Ingrese el total de invitados: "))
-invitados_ca = int(input("Ingrese el total de invitados que beben alcohol: "))
-importe = comida/invitados + bebida_sa/invitados
+try:
+    comida = int(input("Ingrese el total de la comida: "))
+    bebida_sa = int(input("Ingrese el total de la bebida sin alcohol: "))
+    bebida_ca = int(input("Ingrese el total de la bebida con alcohol: "))
+    invitados = int(input("Ingrese el total de invitados: "))
+    invitados_ca = int(input("Ingrese el total de invitados que beben alcohol: "))
+    importe = comida/invitados + bebida_sa/invitados
+    
+    try :
+        extra = bebida_ca/invitados_ca
+    except ZeroDivisionError:
+        extra = 0
+        print("No hay personas que tomen alcohol.")
+    else:
+        print("El importe extra por el consumo de alcohol es: $", extra) 
 
-try :
-    extra = bebida_ca/invitados_ca
-except ZeroDivisionError:
-    extra = 0
-    print("No hay personas que tomen alcohol.")
-else:
-    print("El importe extra por el consumo de alcohol es: $", extra) 
+    print("El importe que debe abonar cada invitado es de: $", importe)
 
-print("El importe que debe abonar cada invitado es de: $", importe)
+except ValueError:
+    print("Los datos ingresados son erroneos."
+          "\nPor favor vuelva a intentarlo solo con valores numéricos")
