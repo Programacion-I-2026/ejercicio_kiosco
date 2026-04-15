583f9bf64fd8c3983d3680b7c86802749efa707e
danilo <danilojosegutierrez020@gmail.comgit config --global user.name>
2026-04-15 09:47:34 -0300

    gutierrez

--- bebida.py
+++ bebida.py
@@ -1,14 +1,18 @@
-comida= int(input("total de la comida:"))
-bebida_sa=int(input("total de la bebida sin alchol:"))
-bebida_ca=int(input("total de la bebida con alchol:"))
-invitados=int(input("cuantos invitados son:"))
-invitados_ca=int(input("cuantos invitados van a tomar alchol:"))
-total=comida/invitados+ bebida_sa/invitados
 try:
-    extra=bebida_ca/invitados_ca
-except ZeroDivisionError:
-    extra=0
-    print("no hay persona que consuma alchol")
-else:
-    print("el importe extra por alchol es de:",extra)
-print("el total de los invitados es de ", total)
+    comida= int(input("total de la comida:"))
+    bebida_sa=int(input("total de la bebida sin alchol:"))
+    bebida_ca=int(input("total de la bebida con alchol:"))
+    invitados=int(input("cuantos invitados son:"))
+    invitados_ca=int(input("cuantos invitados van a tomar alchol:"))
+    total=comida/invitados+ bebida_sa/invitados
+    try:
+        extra=bebida_ca/invitados_ca
+    except ZeroDivisionError:
+        extra=0
+        print("no hay persona que consuma alchol")
+    else:
+        print("el importe extra por alchol es de:",extra)
+        print("el total de los invitados es de ", total)
+except ValueError:
+    print("los datos ingresados son erroneos"
+    "\nPor favor intente solo con valores numericos")
