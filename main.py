1d52071682e261c091143bbcf3716dfb7e6c3880
unknown <rocofoco07@gmail.com>
2026-04-15 08:41:09 -0300

    Ejercicio prueba fiesta de invitados

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-precio = 50
-cantidad = 4
-total = precio * cantidad 
-impuesto = total * 10 / 100
-total_final = total + impuesto
-print(total_final)
+pi = 3.1416
+try:
+    radio = int(input("ingrese el radio:"))
+except ValueError:
+    print("error en el ingreso de numeros")
+else:
+    area = pi*(radio*radio)
+    print ("la superficie del circulo es: ",area)
+finally:
+    print ("Gracias por utlizar el programa")
