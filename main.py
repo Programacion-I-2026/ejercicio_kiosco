66494aea390ecbb8e5fd8089905e5c98425703e7
Angelina Ankovic <angelinaankovic@gmail.com>
2026-04-15 09:04:01 -0300

    solucion problema kiosco

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 precio = 50
 cantidad = 4
-total = precio + cantidad 
-impuesto = total * 10
+total = precio * cantidad #No se debe sumar, se debe multiplicar para saber el total de las 4 unidades
+impuesto = total * 0.10 #Para averiguar porcentaje el 10% se expresa como 0.10
 total_final = total + impuesto
 print(total_final)
