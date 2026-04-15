793e3ce98bd576f10b6b8b43e5c4dda650fdf338
Carolina <carolina.carranzacardoso@gmail.com>
2026-04-15 08:40:30 -0300

    ejercicio prueba bebidas

---
+++ ejercicioprueba.py
@@ -0,0 +1,9 @@
+comida = int(input("Ingrese el total de la comida: "))
+bebida_sa = int(input("Ingrese el total de la bebida sin alcohol: "))
+bebida_ca = int(input("Ingrese el total de la bebida con alcohol: "))
+invitados = int(input("Ingrese el total de invitados: "))
+invitados_ca = int(input("Ingrese el total de invitados que beben alcohol: "))
+importe = comida/invitados+bebida_sa/invitados
+extra = bebida_ca/invitados_ca
+print("El importe que debe abonar cada invitado es de : ", importe)
+print("El importe extra por el consumo de alcohol: ", extra)
