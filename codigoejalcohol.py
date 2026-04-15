838a71bd6d2368f5ed7bb95724ea6b3d6ed478d0
Teromax34 <teromax34@gmail.com>
2026-04-15 09:39:22 -0300

    agregado except ValueError

--- codigoejalcohol.py
+++ codigoejalcohol.py
@@ -1,16 +1,17 @@
-precioComida = int(input("ingrese total de la comida: "))
-precioBebidaSA = int(input("ingrese total de la bebida sin alcohol: "))
-precioBebidaCA = int(input("ingrese total de la bebida con alcohol: "))
-totalInvitados = int(input("ingrese total de invitados: "))
-totalBorrachos = int(input("ingrese total de invitados que beben alcohol: "))
-cuotaInvitados = ((precioComida/totalInvitados)+(precioBebidaSA/totalInvitados))
-try :
-    extraCuotaBorrachos = precioBebidaCA/totalBorrachos
-except ZeroDivisionError:
-    extraCuotaBorrachos = 0
-    print("No hay personas que consuman alcohol")
-else:
-    print("El importe extra por el consmo de alcohol: ", extraCuotaBorrachos)
-print("cada invitado debe pagar: ", cuotaInvitados)
-
-#hola estoy probando mi github este es un cambio pedorro para ver si me anda la ssh key
+try: 
+    precioComida = int(input("ingrese total de la comida: "))
+    precioBebidaSA = int(input("ingrese total de la bebida sin alcohol: "))
+    precioBebidaCA = int(input("ingrese total de la bebida con alcohol: "))
+    totalInvitados = int(input("ingrese total de invitados: "))
+    totalBorrachos = int(input("ingrese total de invitados que beben alcohol: "))
+    cuotaInvitados = ((precioComida/totalInvitados)+(precioBebidaSA/totalInvitados))
+    try :
+        extraCuotaBorrachos = precioBebidaCA/totalBorrachos
+    except ZeroDivisionError:
+        extraCuotaBorrachos = 0
+        print("No hay personas que consuman alcohol")
+    else:
+        print("El importe extra por el consmo de alcohol: ", extraCuotaBorrachos)
+    print("cada invitado debe pagar: ", cuotaInvitados)
+except ValueError:
+    print("los datos ingresados estan mal \n asegurese de ingresar UNICAMENTE NUMEROS")
