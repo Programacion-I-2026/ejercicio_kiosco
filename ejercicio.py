53bfc2ffbaacbc3251d952598641d1e39aac1e5e
unknown <rocofoco07@gmail.com>
2026-04-15 09:43:15 -0300

    Ejercicio Prueba letra error

--- ejercicio.py
+++ ejercicio.py
@@ -1,15 +1,20 @@
-comida = int(input("Ingrese el total de la comida: "))
-bebida_sa = int(input("Ingrese el total de la bebida sin alcohol: "))
-bebida_ca = int(input("Ingrese el total de la bebida con alcohol: "))
-invitados = int(input( "Ingrese el total de invitados: "))
-invitados_ca = int(input("Ingrese el total de invitados que toman alcohol: "))
-importe = comida/invitados+bebida_sa/invitados
 try:
-    extra = bebida_ca/invitados_ca
-except ZeroDivisionError:
-    extra = 0
-    print("No hay personas que tomen alcohol.")
-else:
-    print("El importe extra por el consumo de alcohol es:", extra)
+    comida = int(input("Ingrese el total de la comida: "))
+    bebida_sa = int(input("Ingrese el total de la bebida sin alcohol: "))
+    bebida_ca = int(input("Ingrese el total de la bebida con alcohol: "))
+    invitados = int(input( "Ingrese el total de invitados: "))
+    invitados_ca = int(input("Ingrese el total de invitados que toman alcohol: "))
+    importe = comida/invitados+bebida_sa/invitados
+    try:
+        extra = bebida_ca/invitados_ca
+    except ZeroDivisionError:
+        extra = 0
+        print("No hay personas que tomen alcohol.")
+    else:
+        print("El importe extra por el consumo de alcohol es:", extra)
+        
+    print("El importe que cada invitado debe abonar es de : ", importe)
+except ValueError:
+    print("Los datos ingresados son erroneos."
+           "\nPorfavor vuelva a intentarlo solo con valores numericos")
     
-print("El importe que cada invitado debe abonar es de : ", importe)
