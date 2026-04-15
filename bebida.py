ea5887a3892fbd41d7f96e40e35e40d5bdf6a767
danilo <danilojosegutierrez020@gmail.comgit config --global user.name>
2026-04-15 09:22:30 -0300

    gutierrez

--- bebida.py
+++ bebida.py
@@ -4,6 +4,11 @@ bebida_ca=int(input("total de la bebida con alchol:"))
 invitados=int(input("cuantos invitados son:"))
 invitados_ca=int(input("cuantos invitados van a tomar alchol:"))
 total=comida/invitados+ bebida_sa/invitados
-extra=bebida_ca/invitados_ca
+try:
+    extra=bebida_ca/invitados_ca
+except ZeroDivisionError:
+    extra=0
+    print("no hay persona que consuma alchol")
+else:
+    print("el importe extra por alchol es de:",extra)
 print("el total de los invitados es de ", total)
-print("el extra es de:", extra)
