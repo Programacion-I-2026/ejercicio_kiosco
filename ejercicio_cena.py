bac2ba774f0b60f74b2458482066dd8845e2649f
martin peralta <martinperalta2593@gmail.com>
2026-04-15 09:49:07 -0300

    ejercicio corregido

--- ejercicio_cena.py
+++ ejercicio_cena.py
@@ -1,15 +1,18 @@
-comida=int(input("Ingrese los gastos en comida: "))
-invitados=int(input("Ingrese cuantos invitados van a la cena: "))
-sinAlcohol=int(input("Ingrese los gastos en bebidas sin alcohol: "))
-alcohol=int(input("Ingrese los gastos en bebidas con alcohol: "))
-invAlcohol=int(input("Ingrese la cantidad de invitados que consumen alcohol: "))
-comybeb= (comida + sinAlcohol) / invitados
 try:
-    borrachos= comybeb + (alcohol/invAlcohol)
-except ZeroDivisionError:
-    borrachos= 0
-    print("No hay invitados que tomen alcohol")
-else:
-    print(f"si toma alcohol debera pagar: {borrachos}")
-print(f"Cada uno debera pagar: {comybeb}")
-print("gracias por usar nuestro servicio!")
+    comida=int(input("Ingrese los gastos en comida: "))
+    invitados=int(input("Ingrese cuantos invitados van a la cena: "))
+    sinAlcohol=int(input("Ingrese los gastos en bebidas sin alcohol: "))
+    alcohol=int(input("Ingrese los gastos en bebidas con alcohol: "))
+    invAlcohol=int(input("Ingrese la cantidad de invitados que consumen alcohol: "))
+    comybeb= (comida + sinAlcohol) / invitados
+    try:
+        borrachos= comybeb + (alcohol/invAlcohol)
+    except ZeroDivisionError:
+        borrachos= 0
+        print("No hay invitados que tomen alcohol")
+    else:
+        print(f"si toma alcohol debera pagar: {borrachos}")
+        print(f"Cada uno debera pagar: {comybeb}")
+        print("gracias por usar nuestro servicio!")
+except ValueError:
+    print("Ingrese solo numeros")
