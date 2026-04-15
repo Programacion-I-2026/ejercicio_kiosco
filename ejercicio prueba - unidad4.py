8e4ee46daae4ac4396dba7691cc4d8cf31e58edc
Ludmila Depetris <ludmilamdepetris@gmail.com>
2026-04-15 09:48:45 -0300

    ejercicioprueba2026

--- ejercicio prueba - unidad4.py
+++ ejercicio prueba - unidad4.py
@@ -4,22 +4,30 @@ print("-------------------------------------")
 print(" Ludmila Depetris, Comision 4 . . . .")
 
 # INGRESO DE DATOS:
-totalComida=int(input("Total gastado en comida: "))
-total_bebidas_sA=int(input("Total bebidas sin alcohol: "))
-total_bebida_cA=int(input("Total bebidas con alcohol: "))
-cantInvitados=int(input("Cantidad de invitados a asistir: "))
-cantInvitadosA=int(input("Cantidad de invitados que cosumieron bebidas alcoholicas: "))
+try:
+    totalComida=int(input("Total gastado en comida: "))
+    total_bebidas_sA=int(input("Total bebidas sin alcohol: "))
+    total_bebida_cA=int(input("Total bebidas con alcohol: "))
+    cantInvitados=int(input("Cantidad de invitados a asistir: "))
+    cantInvitadosA=int(input("Cantidad de invitados que cosumieron bebidas alcoholicas: "))
 
-# ENTRADA GENERAL:
-entradaGeneral= totalComida+total_bebidas_sA / cantInvitados
-print("El total que debe pagar cada persona es de:",entradaGeneral)
-print("Costo sin consumision de alcohol.")
+    # ENTRADA GENERAL:
+    entradaGeneral= totalComida+total_bebidas_sA / cantInvitados
+    print("El total que debe pagar cada persona es de:",entradaGeneral)
+    print("Costo sin consumision de alcohol.")
 
-
-# COSTO EXTRA POR ALCOHOL:
-extra= total_bebida_cA / cantInvitadosA
-entradaAlcohol= entradaGeneral+extra
-
+    try:
+     extra= total_bebida_cA / cantInvitadosA
+    except ZeroDivisionError:
+        extra=0
+        print("No hay personas que consuman alcohol.")
+    else:
+        # COSTO EXTRA POR ALCOHOL:
+        extra= total_bebida_cA / cantInvitadosA
+        entradaAlcohol= entradaGeneral+extra
+except ValueError:
+    print("Los datos son erroneos. No se puede ingresar letras.")
+    print("\n Porfavor vuelva a internarlo.")
 
 print("El costo a pagar es de:",entradaGeneral,"y agregando el alcohol es:",round(extra,2))
 print("El costo de la entrada con alcohol es de:",round(entradaAlcohol,2))
